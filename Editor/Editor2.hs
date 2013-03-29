240addd0a6867916fdb2ab56206398a158790e51
Luze26 <yann.uzel@gmail.com>
2013-03-30 00:30:51 +0100

    cleaner repo

--- Editor/Editor2.hs
+++ Editor/Editor2.hs
@@ -8,10 +8,10 @@ main = do
 	start (args ++ (createList 700))
 
 start :: [String] -> IO()
-start (file:m:args) = writeFile file $ generate args $ readInt m
+start (file:m:args) = writeFile file ("[\"Topic\",\"Editor\",\"Section\",\"Editor\"]\n\nscript=" ++ (printTree True "" $ generate args $ readInt m))
 
-generate :: [String] -> Int -> String
-generate n m = show (Node ("Root","1","null",[]) $ editorLevel (length n) n n m 0 :: NTree Cell)
+generate :: [String] -> Int -> NTree Cell
+generate n m = Node ("Root","1","null",[]) $ editorLevel (length n) n n m 0
 
 editorLevel :: Int -> [String] -> [String] -> Int -> Int -> [NTree Cell]
 editorLevel _ _ [] _ _ = []
@@ -37,6 +37,23 @@ sectionLevel eId tId nb n m i = Node ("Section", secId, eId, [ "Section" ++ tId 
 
 readInt :: String -> Int
 readInt = read
+
+printTree :: Bool -> String -> NTree Cell -> String
+printTree noComma tabs (Node cell subtrees) = tabs ++ "Node " ++ (show cell) ++ starting ++ (printSubTrees ('\t':tabs) subtrees) ++ ending
+																						where
+																							starting
+																								| subtrees == [] = " ["
+																								| otherwise = " [\n"
+																							ending
+																								| noComma = "]"
+																								| otherwise = "],\n"
+
+printSubTrees :: String -> [NTree Cell] -> String
+printSubTrees _ [] = ""
+printSubTrees tabs (s:subtrees) = printTree noComma tabs s ++ (printSubTrees tabs subtrees)
+																					where
+																						noComma = subtrees == []
+
 
 createList :: Int -> [String]
 createList 1 = ["e1"]
