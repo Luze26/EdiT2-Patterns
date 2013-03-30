71a275b04e915ed76dfd34ab1c2b999ab060134f
Luze26 <yann.uzel@gmail.com>
2013-03-30 01:07:09 +0100

    clean4

--- test.hx
+++
@@ -1,3 +0,0 @@
-module Main
-(Var "main",Value {args = 0, fixity = Def, priority = 9, letBound = True, traced = True})
-(Var "repartition",Value {args = 5, fixity = Def, priority = 9, letBound = True, traced = True})
