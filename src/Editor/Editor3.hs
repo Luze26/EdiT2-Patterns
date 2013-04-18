126449602a3028313e9920889593e4d379e32903
Luze26 <yann.uzel@gmail.com>
2013-04-18 16:59:01 +0200

    fix genric

--- src/Editor/Editor3.hs
+++ src/Editor/Editor3.hs
@@ -9,19 +9,20 @@ import TreeGenerator
 main :: IO()
 main = do
 	args <- getArgs
-	start (args ++ [createList 20])
+	start (args ++ [createList 6])
 
 
 
 -- start, parse arguments and write generated tree in a file
 -- @[String] -> args
 start :: [String] -> IO()
-start (file:nbSec:e:ps:_) = writeFile file $ showTree ["Topic","Editor","Section","Editor2"] $ generate $ generateLevels (read ps::[String]) (readInt nbSec) $ readInt e
+start (file:nbSec:e:ps:_) = writeFile file $ showTree ["Topic","Editor","Section","Editor2"]  $ generate $ generateLevels (read ps::[String]) (readInt nbSec) $ readInt e
 
 
 
 generateLevels :: [String] -> Int -> Int -> [Level]
-generateLevels participants nbSec nbSecEditor = generateTopics participants : generateLvlEditor participants
+generateLevels participants nbSec nbSecEditor = generateLvlTopics participants :
+		 generateLvlEditor participants : generateLvlSection participants nbSec : generateLvlEditor2 participants nbSecEditor nbSec : []
 
 generateLvlTopics :: [Participant] -> Level
 generateLvlTopics participants = ("Topic", generateLvlTopics' participants 1)
@@ -35,7 +36,7 @@ generateLvlEditor participants = ("Editor", generateLvlEditor' participants)
 
 generateLvlEditor' :: [Participant] -> [[[String]]]
 generateLvlEditor' [] = []
-generateLvlEditor' p:ps = [[p]] : generateLvlEditor' ps
+generateLvlEditor' (p:ps) = [[p]] : generateLvlEditor' ps
 
 generateLvlSection :: [Participant] -> Int -> Level
 generateLvlSection participants nbS = ("Section", generateLvlSection' participants nbS 1)
@@ -48,10 +49,24 @@ generateLvlSection' (_:ps) nbS i = generateLvlSection'' nbS i 1 : (generateLvlSe
 
 generateLvlSection'' :: Int -> Int -> Int -> [[String]]
 generateLvlSection'' nbS i i2
-	| i2 <= nbS = ["Section" ++ (div i nbS) ++ (show i2)] : generateLvlSection'' nbS i (i2+1)
+	| i2 <= nbS = ["Section" ++ (show i ) ++ (show i2)] : generateLvlSection'' nbS i (i2+1)
 	| otherwise = []
 
 
---generateLvlEditor2 :: [Participant] -> Level
---generateLvlEditor participants = ("Editor2", )
+generateLvlEditor2 :: [Participant] -> Int -> Int -> Level
+generateLvlEditor2 participants nbE nbSec = ("Editor2", generateLvlEditor2' nbE nbSec participants $ take ((nbE+3)*(length participants)) $ cycle participants)
 
+
+generateLvlEditor2' :: Int -> Int -> [Participant] -> [Participant] -> [[[Participant]]]
+generateLvlEditor2' _ _ [] _ = []
+generateLvlEditor2' nbE nbSec (p:ps) participants = (map (\e -> [e]) editors) : (map (\e -> [e]) editors2) : generateLvlEditor2' nbE nbSec ps ps3
+	where
+		(editors, ps2) = takeParticipants participants p nbE
+		(editors2, ps3) = takeParticipants ps2 p nbE
+
+
+takeParticipants :: [Participant] -> Participant -> Int -> ([Participant], [Participant])
+takeParticipants ps _ 0 = ([],ps)
+takeParticipants (p:ps) p2 nbE
+	| p /= p2 = let (editors, ps2) = takeParticipants ps p2 (nbE-1) in (p:editors,ps2)
+	| otherwise = takeParticipants ps p2 nbE
--- src/Util/TreeGenerator.hs
+++ src/Util/TreeGenerator.hs
@@ -16,18 +16,29 @@ type Level = (Name, [[[String]]])
 -- @[Level] -> list of levels
 -- @NTree Cell -> the tree
 generate  :: [Level] -> NTree Cell
-generate lvls = Node ("Root","1","null",[]) $ generate' lvls "1" 1
+generate (l:lvls) = Node ("Root","1","null",[]) $ generate' l lvls "1" 1
 
 
-
--- generate', generate subtree given the levels
--- @[Level] -> levels
--- @String -> father numbering
--- @Int -> sub id
--- @[NTree Cell] -> subtrees
-generate' :: [Level] -> String -> Int -> [NTree Cell]
-generate' [] _ _ = []
-generate' ((_, []:_):_) _ _ = []
-generate' ((name, (r:row):rows):lvls) pid i =  (Node (name, id, pid, r) $ generate' lvls id 1) : generate' ((name, row:rows):lvls) pid (i+1) 
+generate' :: Level -> [Level] -> String -> Int -> [NTree Cell]
+generate' (_, []) _ _ _ = []
+generate' (name, [r]:rows) lvls pid i = (Node (name, id, pid, r) subtree) : generate' (name, rows) ls pid (i+1)
 	where
 		id = pid ++ (show i)
+		(ls, subtree)
+			| lvls == [] = ([], [])
+			| otherwise = generate'' (head lvls) (tail lvls) id 1
+
+
+
+generate'' :: Level -> [Level] -> String -> Int -> ([Level], [NTree Cell])
+generate'' (name, []:rows) lvls _ _ = ((name,rows):lvls, [])
+generate'' (name, []) lvls _ _ = ((name,[]):lvls, [])
+generate'' (name, (r:row):rows) lvls pid i = (ls, (Node (name, id, pid, r) subtree):nexttree)
+	where
+		id = pid ++ (show i)
+		(lsub, subtree)
+			| lvls == [] = ([], [])
+			| otherwise = generate'' (head lvls) (tail lvls) id 1
+		(ls, nexttree) = generate'' (name, row:rows) lsub pid (i+1)
+		
+		
