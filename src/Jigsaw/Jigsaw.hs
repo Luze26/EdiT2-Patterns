ecc615b36754f7c834c685350f1952068f4a9c57
Luze26 <yann.uzel@gmail.com>
2013-05-29 16:38:25 +0200

    jigsaw

---
+++ src/Jigsaw/Jigsaw.hs
@@ -0,0 +1,275 @@
+import Data.Char( intToDigit )
+import Util
+import TreeGenerator
+import JigsawModel
+import qualified JigsawPatternInformation as Info
+
+
+
+-- main, entry point
+-- write the generated tree into a file
+main :: IO()
+main
+	| notPossible == [] = writeT2 Info.file ["Activities", "Groups", "Participants", "Resources"] (generate generateLevels)
+		(Info.activityObjectsList, Info.groupObjectsList, Info.participantObjectsList, Info.resourceObjectsList, []) Info.teacherNotes
+	| otherwise = writeT2Err Info.file notPossible
+
+
+
+-- notPossible, return the list of not possible actions
+-- @[Possible String] -> NotPossible actions
+notPossible :: [Possible String]
+notPossible = foldl (\acc x -> case x of NotPossible msg -> NotPossible msg : acc; Possible _ -> acc) [] (repartitionJigsaw : repartitionInitial)
+
+
+
+-- generateLevels, generatee the levels of the tree
+-- @[Level] -> levels
+generateLevels :: [Level]
+generateLevels = generateActivitiesLvl : generateGroupsLvl : generateParticipantsLvl : generateResourcesLvl : []
+
+
+
+-- //////////////////////////////////// ACTIVITIES ////////////////////////////////////
+
+-- generateActivitiesLvl, generate activities level
+-- @Level -> activities level
+generateActivitiesLvl :: Level
+generateActivitiesLvl = ("Activities", [[["Initial"]], [["Expert"]], [["Jigsaw"]]]) 
+
+
+
+-- //////////////////////////////////// GROUPS ////////////////////////////////////
+
+-- generateGroupsLvl, generate groups level
+-- @Level -> groups level
+generateGroupsLvl :: Level
+generateGroupsLvl = ("Groups", createGroupsInitial : createGroupsExpert : createGroupsJigsaw : [])
+
+
+
+-- createGroupsInitial, create initial groups
+-- @[[Group]] -> initial groups
+createGroupsInitial :: [[Group]]
+createGroupsInitial = [["Group " ++ show i] | i <- [1..nbGroupsInitial]]
+
+
+
+-- createGroupsExpert, create expert groups
+-- @[[Group]] -> expert groups
+createGroupsExpert :: [[Group]]
+createGroupsExpert = [["Group Expert " ++ (name t)] | t <- Info.themes]
+
+
+
+-- createGroupsJigsaw, create jigsaw groups
+-- @[[Group]] -> jigsaw groups
+createGroupsJigsaw :: [[Group]]
+createGroupsJigsaw = [["Group Jigsaw " ++ (show i)] | i <- [1..(length createParticipantsJigsaw)]]
+
+
+
+-- //////////////////////////////////// PARTICIPANTS ////////////////////////////////////
+
+generateParticipantsLvl :: Level
+generateParticipantsLvl = ("Participants", map (\x -> map (\y -> [y]) x) (createParticipantsInitial ++ createParticipantsExpert ++ createParticipantsJigsaw))
+
+
+
+-- createParticipantsInitial, split participants into initial groups
+-- @[InitialGroup] -> initial groups
+createParticipantsInitial :: [InitialGroup]
+createParticipantsInitial = map (\(_,x) -> x) createParticipantsInitialWithTheme
+
+
+
+
+-- createParticipantsInitialWithTheme, split participants into initial groups
+-- @[(Theme, InitialGroup)] -> initial groups with their associated theme
+createParticipantsInitialWithTheme :: [(Theme, InitialGroup)]
+createParticipantsInitialWithTheme = createParticipantsInitial' createParticipantsExpert Info.themes repartitionInitial''
+
+
+
+-- createParticipantsInitial', split participants into initial groups from expert groups
+-- @[ExpertGroup] -> expert groups
+-- @[Theme] -> themes
+-- @[(Theme, InitialGroup)] -> initial groups with their associated theme
+createParticipantsInitial' :: [ExpertGroup] -> [Theme] -> [[Int]] -> [(Theme, InitialGroup)]
+createParticipantsInitial' [] _ _ = []
+createParticipantsInitial' (g:groups) (t:themes) (r:rs) = map (\x -> (t,x)) (splitParticipants g r) ++ createParticipantsInitial' groups themes rs
+
+
+
+-- createParticipantsExpert, split participants into expert groups
+-- Merge participants splited per themes for each jigsaw group, into a list of participant splited only per themes
+-- @[ExpertGroup] -> expert groups
+createParticipantsExpert :: [ExpertGroup]
+createParticipantsExpert = foldl (\acc x -> addAll acc x) (replicate nbThemes []) (createParticipantsExpert' createParticipantsJigsaw)
+
+
+
+-- addAll, merge on elements of 2 list
+-- @[[Participant]] -> list 1
+-- @[[Participant]] -> list 2
+-- @[[Participant]] -> merged list
+addAll :: [[Participant]] -> [[Participant]] -> [[Participant]]
+addAll [] _ = []
+addAll (a:acc) (t:ts) = (a ++ t) : addAll acc ts
+
+
+
+-- createParticipantsExpert', split jigsaw groups per themes
+-- @[JigsawGroup] -> repartition for jigsaw groups
+-- @[[[Participant]]] -> participants splited per jigsaw groups and per themes
+createParticipantsExpert' :: [JigsawGroup] -> [[[Participant]]]
+createParticipantsExpert' [] = []
+createParticipantsExpert' (p:ps) = createParticipantsExpert'' Info.themes p : createParticipantsExpert' ps
+
+
+
+-- createParticipantsExpert'', split a Jigsaw group between themes
+-- @[Theme] -> themes
+-- @JigsawGroup-> participant of one jigsaw group
+-- @[[Participant]] -> participants splited between themes
+createParticipantsExpert'' :: [Theme] -> JigsawGroup -> [[Participant]]
+createParticipantsExpert'' [] _ = []
+createParticipantsExpert'' (t:themes) ps = first : createParticipantsExpert'' themes rest
+	where
+		(first, rest) = splitAt (nbExpert t) ps
+
+
+
+-- createParticipantsJigsaw, split participants into groups jigsaw
+-- @[JigsawGroup] -> jigsaw groups
+createParticipantsJigsaw :: [JigsawGroup]
+createParticipantsJigsaw = case repartitionJigsaw of
+								NotPossible msg -> []
+								Possible list ->  splitParticipants participants list
+
+
+-- //////////////////////////////////// RESOURCES ////////////////////////////////////
+
+-- generateResourcesLvl, generate resources
+-- @Level -> the level resources
+generateResourcesLvl :: Level
+generateResourcesLvl = ("Resources", map (\x -> map (\y -> [y]) x) (createResourcesInitial ++ createResourcesExpert ++ createResourcesJigsaw))
+
+
+
+-- createResourcesInitial, create resources list for the expert level
+-- @[[String]] -> resources
+createResourcesInitial :: [[String]]
+createResourcesInitial = foldl (\acc (t,ps) -> acc ++ (map (\p -> resources t) ps)) [] createParticipantsInitialWithTheme
+
+
+
+-- createResourcesExpert, create resources list for the expert level
+-- @[[String]] -> resources
+createResourcesExpert :: [[String]]
+createResourcesExpert = createResourcesExpert' Info.themes createParticipantsExpert
+
+
+
+-- createResourcesExpert', create resources for the expert level
+-- @[Theme] -> themes
+-- @[ExpertGroup] -> Expert groups
+-- @[[String]] -> resources
+createResourcesExpert' :: [Theme] -> [ExpertGroup] -> [[String]]
+createResourcesExpert' [] _ = []
+createResourcesExpert' (t:themes) (g:groups) = map (\p -> resourcesTheme) g ++ createResourcesExpert' themes groups
+	where
+		resourcesTheme = resources t
+
+
+-- createResourcesJigsaw, create resources list for the jigsaw level
+-- @[[String]] -> resources
+createResourcesJigsaw :: [[String]]
+createResourcesJigsaw = replicate nbParticipants allResources
+
+
+
+-- //////////////////////////////////// UTIL ////////////////////////////////////
+
+repartitionInitial :: [Possible [Int]]
+repartitionInitial = repartitionInitial' createParticipantsExpert Info.themes
+
+
+
+-- repartitionInitial', list of size for jigsaw's group
+-- @[Possible [Int]] -> list of size for jigsaw's group
+repartitionInitial' :: [ExpertGroup] -> [Theme] -> [Possible [Int]]
+repartitionInitial' [] _ = []
+repartitionInitial' (g:groups) (t:themes) =
+	case repartition (length g) Info.nbPPG Info.above Info.below of
+		NotPossible msg -> NotPossible ("Can't do a good reparition for initial group on the theme " ++ (name t)) : repartitionInitial' groups themes
+		Possible list -> Possible list : repartitionInitial' groups themes
+
+
+
+repartitionInitial'' :: [[Int]]
+repartitionInitial'' = repartitionInitial''' repartitionInitial
+
+
+
+repartitionInitial''' :: [Possible [Int]] -> [[Int]]
+repartitionInitial''' [] = []
+repartitionInitial''' (r:rs) =
+	case r of
+		NotPossible _ -> []
+		Possible list -> list : repartitionInitial''' rs
+
+
+
+-- repartitionJigsaw, list of size for jigsaw's group
+-- @Possible [Int] -> list of size for jigsaw's group
+repartitionJigsaw :: Possible [Int]
+repartitionJigsaw =
+	case repartition (length participants) nbPPGJ upperMarginJigsaw lowerMarginJigsaw of
+		NotPossible _ -> NotPossible "Can't do a good reparition for jigsaw groups"
+		Possible list -> Possible list
+	where
+		(nbPPGJ, upperMarginJigsaw, lowerMarginJigsaw) = nbParticipantJigsaw
+
+
+
+-- allResources, create a list with all the resources
+-- @[String] -> resources
+allResources :: [String]
+allResources = foldl (\acc t -> acc ++ (resources t)) [] Info.themes
+
+
+
+-- nbParticipantJigsaw, give information on the repartition wanted for jigsaw groups
+-- @(Int, Int, Int) -> (number of participants wanted for a jigsaw group, how many participants additional are tolerated, 
+-- 						how many participants less are tolerated)
+nbParticipantJigsaw :: (Int, Int, Int)
+nbParticipantJigsaw = foldl (\(x,y,z) theme -> (x + nbExpert theme, y + upperMargin theme, z + lowerMargin theme)) (0, 0, 0) Info.themes
+
+
+
+-- nbThemes, number of themes
+-- @Int -> number of themes
+nbThemes :: Int
+nbThemes = length Info.themes
+
+
+
+-- nbGroupInitial, number of initial group
+-- @Int -> number of initial group
+nbGroupsInitial :: Int
+nbGroupsInitial = length createParticipantsInitial
+
+
+
+-- nbParticipants, number of participants
+-- @Int -> number of participants
+nbParticipants :: Int
+nbParticipants = length participants
+
+
+
+-- participants, list of participants logins
+-- @[Participant] -> list of participants logins
+participants :: [Participant]
+participants = participantsLogins Info.participantObjectsList
---
+++ src/Jigsaw/JigsawModel.hs
@@ -0,0 +1,22 @@
+module JigsawModel where
+
+import Util
+
+-- Initial Group
+type InitialGroup = [Participant]
+
+-- Expert Group
+type ExpertGroup = [Participant]
+
+-- Jigsaw Group
+type JigsawGroup = [Participant]
+
+-- Theme
+data Theme =
+	Theme {
+		name :: String,				-- name of the theme
+		resources :: [Resource],	-- resources of the theme
+		nbExpert :: Int,			-- number of expert for the theme needed in a Jigsaw group
+		lowerMargin :: Int,			-- number of additionnal experts allowed in a Jigsaw group
+		upperMargin :: Int			-- number of less experts allowed in a Jigsaw group
+	}
